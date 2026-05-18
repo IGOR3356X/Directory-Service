84b168771f45f9ddb2b3c2e8498656b982e6535e
NetherKick <igor3356c@gmail.com>
2026-05-18 18:04:17 +0300

    Update

--- backend/DirectoryService/src/DirectoryService.Domain/Departament.cs
+++ backend/DirectoryService/src/DirectoryService.Domain/Departament.cs
@@ -2,15 +2,23 @@
 
 public class Departament
 {
-    private List<DepartmentLocation> _departmentLocations;
+    public Departament(Guid id, string name, string slug, string path, Guid parentId, bool isActive)
+    {
+        Id = id;
+        Name = name;
+        Slug = slug;
+        Path = path;
+        ParentId = parentId;
+        IsActive = isActive;
+        CreatedAt = DateTime.UtcNow;
+    }
+
     public Guid Id { get; private set; }
     public string Name { get; private set; }
-    public string Identifier { get; private set; } 
-    public Guid ParentId  { get; private set; }
-    public string Path  { get; private set; }
-    public int Depth { get; private set; }
+    public string Slug { get; private set; }
+    public string Path { get; private set; }
+    public Guid ParentId { get; private set; }
     public bool IsActive { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime UpdatedAt { get; private set; }
-    public IReadOnlyCollection<DepartmentLocation>  DepartmentLocations => _departmentLocations;
 }
--- backend/DirectoryService/src/DirectoryService.Domain/Location.cs
+++ backend/DirectoryService/src/DirectoryService.Domain/Location.cs
@@ -5,7 +5,6 @@ public class Location
     public Guid Id { get; private set; }
     public string Name { get; private set; }
     public string Address { get; private set; }
-    public string Timezone { get; private set; }
     public bool IsActive { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime UpdatedAt { get; private set; }
