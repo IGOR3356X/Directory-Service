b10656191e45f8555cfb396cfb88a750066089c8
NetherKick <igor3356c@gmail.com>
2026-05-27 17:53:40 +0300

    start work

---
+++ backend/DirectoryService/src/DirectoryService.Infrastructure.Postgres/Configuration/DepartmentConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using DirectoryService.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Path = DirectoryService.Domain.Path;
+
+namespace DirectoryService.Infrastructure.Postgres.Configuration;
+
+public class DepartmentConfiguration: IEntityTypeConfiguration<Department>
+{
+    public void Configure(EntityTypeBuilder<Department> builder)
+    {
+        builder.ToTable("department");
+        
+        builder.HasKey(x => x.Id).HasName("id");
+        
+        builder
+            .Property<Name>(x=> x.Name)
+            .HasConversion(n => n.ToString(),fdt => Name.Create(fdt))
+            .HasMaxLength(250)
+            .IsRequired();
+        
+        builder
+            .Property(x => x.Slug)
+            .HasConversion(x => x.ToString(), fdt => Slug.Create(fdt))
+            .HasMaxLength(150)
+            .IsRequired();
+        
+        builder
+            .Property(x => x.Path)
+            .HasConversion(x => x.ToString(), fdt => Path.FromDb(fdt))
+            .HasMaxLength(150)
+            .IsRequired();
+        
+        
+    }
+}
