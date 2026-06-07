214466dba8dc119520256236c74e53a7d2f138ca
NetherKick <igor3356c@gmail.com>
2026-06-07 14:00:53 +0300

    Micro pravki

--- backend/DirectoryService/src/DirectoryService.Core/Location/CreateLocationValidator.cs
+++ backend/DirectoryService/src/DirectoryService.Core/Location/CreateLocationValidator.cs
@@ -7,7 +7,10 @@ public class CreateLocationValidator : AbstractValidator<CreateLocationDto>
 {
     public CreateLocationValidator()
     {
-        RuleFor(x => x.Name).NotNull().NotEmpty().MinimumLength(3).MaximumLength(255).WithMessage("Имя локации не может быть пустым");
+        RuleFor(x => x.Name)
+            .NotNull().NotEmpty().WithMessage("Имя локации не может быть пустым")
+            .MinimumLength(3).WithMessage("Имя должно быть не короче 3 символов")
+            .MaximumLength(255).WithMessage("Имя должно быть не длиннее 255 символов");
         RuleFor(x => x.City).NotNull().NotEmpty().WithMessage("Город не может быть пустым");
         RuleFor(x => x.Street).NotNull().NotEmpty().WithMessage("Улица не может быть пустой");
         RuleFor(x => x.HouseNumber).NotNull().NotEmpty().WithMessage("Дом не может быть пустым");
