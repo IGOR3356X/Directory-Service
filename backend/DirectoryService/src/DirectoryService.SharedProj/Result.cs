c33430e4ecbbba68dd9e3b7e8d9a7c269dc39abe
NetherKick <igor3356c@gmail.com>
2026-09-03 12:28:09 +0300

    Save

--- backend/DirectoryService/src/DirectoryService.SharedProj/Result.cs
+++ backend/DirectoryService/src/DirectoryService.SharedProj/Result.cs
@@ -4,7 +4,7 @@ public class Result
 {
     public bool IsSuccess { get; }
 
-    public bool IsFalilure => !IsSuccess;
+    public bool IsFailure => !IsSuccess;
 
     public Errors Errors { get; set; }
 
@@ -27,15 +27,19 @@ public class Result
 #pragma warning disable CA1000
 public sealed class Result<TValue>: Result
 {
-    private Result(TValue value) => Value = value;
+    private readonly TValue _value = default!;
+    private Result(TValue value) => _value = value;
     private Result(Errors error) : base(error){}
     
     public new static Result<TValue> Failure(Errors error) => new(error);
 
     public static Result<TValue> Success(TValue value) => new (value);
-    
-    public TValue Value
-    {
-        get => IsSuccess ? field : throw new InvalidOperationException("Result is not success");
-    } = default!;
+
+    // public static implicit operator Result<TValue>(Errors error) => Failure(error);
+    //
+    // public static implicit operator Result<TValue>(TValue value) => Success(value);
+    //
+    // public static implicit operator TValue(Result<TValue> result) => result._value;
+    //
+    // private readonly TValue Value = IsSuccess ? _value : throw new InvalidOperationException();
 }
